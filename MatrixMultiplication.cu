5fe1ccc27943f7a6d914530da26e162c77f57d22
ajinkyamalhotra <35980852+ajinkyamalhotra@users.noreply.github.com>
2018-06-14 11:20:48 -0700

    Update MatrixMultiplication.cu

--- MatrixMultiplication.cu
+++ MatrixMultiplication.cu
@@ -20,30 +20,30 @@
   } while (0)
 
 
-//Matrix multiplication kernel
+//@@Matrix multiplication kernel
 __global__ void matrixMultiply(float *A, float *B, float *C, int numARows, int numAColumns, int numBRows, int numBColumns, int numCRows, int numCColumns) {
 	
-	//Y-axis Thread Dimension
+	//@@Y-axis Thread Dimension
 	int row = blockIdx.y*blockDim.y + threadIdx.y;
 
-	//X-axis Thread Dimension
+	//@@X-axis Thread Dimension
 	int columns = blockIdx.x*blockDim.x + threadIdx.x;
 
-	// Initilaizing final value to add in the output matrix
+	//@@Initilaizing final value to add in the output matrix
 	float pValue = 0;
 
-	//checking for boundary condition 
+	//@@checking for boundary condition 
 	if (row < numARows  && columns < numBColumns) {
 		
-		//adding values from 0 to matrix A width and from 0 to martrix B Height
+		//@@adding values from 0 to matrix A width and from 0 to martrix B Height
 		for (int k = 0; k < numAColumns; k++) {
 		
-			//Summation of the rows from matrix A and columns from matrix B to pValue
+			//@@Summation of the rows from matrix A and columns from matrix B to pValue
 			pValue += A[row*numAColumns + k] * B[k*numBColumns + columns];
 		
 		}
 		
-		//add the final value to the output matrix
+		//@@add the final value to the output matrix
 		C[row*numCColumns + columns] = pValue;
 	
 	}
